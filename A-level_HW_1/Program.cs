6df1f60bfba7a951447746fa3e854f58e578db1f
Владислав Павлов <vlpavlov58@gmail.com>
2018-09-16 13:27:02 +0300

    Add project files.

---
+++ A-level_HW_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_level_HW_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
